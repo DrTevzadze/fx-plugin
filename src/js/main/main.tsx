f3e6544fa281496e82a49a27dabda49adeae9993
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-11-07 16:10:49 +0400

    Setup global.css. Add new font. Start navigation bar

--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -1,6 +1,9 @@
+import Navigation from "./components/navigation/Navigation";
+
 const Main = () => {
   return (
     <div>
+      <Navigation />
       <h1>Welcome to Bolt CEP! This is a test</h1>
     </div>
   );
